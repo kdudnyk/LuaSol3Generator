97e924c2a215c3670fb249ec40f79b8bec7f8839
Kostiantyn Dudnyk <kdudnykk@gmail.com>
2019-06-06 18:01:17 +0300

    Safe to separate files

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from mako.template import Template
 import sys
 import clang.cindex
 import ntpath
+import os
+from pathlib import Path
 
-clang.cindex.Config.set_library_file('/usr/lib/x86_64-linux-gnu/libclang-9.so.1')
+# clang.cindex.Config.set_library_file('/usr/lib/x86_64-linux-gnu/libclang-9.so.1')
+clang.cindex.Config.set_library_path('/usr/lib/x86_64-linux-gnu/')
 index = clang.cindex.Index.create()
 
 global_registry = []
-registry = []
+# registry = {}
+registries = {}
 free_functions = {}
 
 class Method:
@@ -25,7 +29,9 @@ def filter_node_list_by_classes(
     global free_functions
 
     for i in nodes:
+        filename = Path(i.location.file.name).stem
         if i.kind in [clang.cindex.CursorKind.CLASS_DECL,clang.cindex.CursorKind.STRUCT_DECL]:
+
             if i.is_abstract_record():
                 continue
             if i.is_definition():
@@ -34,16 +40,31 @@ def filter_node_list_by_classes(
                 usertype_methods = {}
                 usertype_constructors = []
                 usertype_meta_functions = []
+                usertype_parents = []
 
                 for child in i.get_children():
                     if child.access_specifier != clang.cindex.AccessSpecifier.PUBLIC:
                         continue
 
                     if child.kind == clang.cindex.CursorKind.FIELD_DECL:
-                        # we are currently interested in only public fields which are non pointers
-                        if child.access_specifier == clang.cindex.AccessSpecifier.PUBLIC and child.type.kind != clang.cindex.TypeKind.POINTER:
-                            usertype_fields.append(child.displayname)
+                        if child.type.kind == clang.cindex.TypeKind.POINTER:
+                            # print("potentially skipping {}".format(child.spelling))
+                            skip = False
+                            for chill in child.get_children():
+                                # print("chill {}, {}".format(chill.displayname,chill.type.kind))
+                                if chill.type.kind == clang.cindex.TypeKind.TYPEDEF:
+                                    skip = True
+                                    continue
+                            # print("Skiiping ? {}".format(skip))
+                            if skip:
+                                continue
 
+                        # we are currently interested in only public fields which are non pointers
+                        if child.access_specifier == clang.cindex.AccessSpecifier.PUBLIC:
+                                # and child.type.kind != clang.cindex.TypeKind.POINTER:
+                            usertype_fields.append(child.displayname)
+                    elif child.kind == clang.cindex.CursorKind.CXX_BASE_SPECIFIER:
+                        usertype_parents.append(child.type.spelling)
                     elif child.kind == clang.cindex.CursorKind.CXX_METHOD:
                         # skip operators for now
                         if child.spelling.startswith("operator"):
@@ -68,7 +89,6 @@ def filter_node_list_by_classes(
                             elif child.canonical.spelling == "operator-=":
                                 usertype_meta_functions.append(("-=", "subtraction",args))
                             continue
-
                         method = Method(child.spelling)
                         signature = child.type.get_result().spelling + child.displayname[child.displayname.find('('):child.displayname.find(')')+1]
                         if child.is_const_method():
@@ -86,11 +106,15 @@ def filter_node_list_by_classes(
                         signature = child.displayname[child.displayname.find('('):child.displayname.find(')') + 1]
                         usertype_constructors.append(signature)
 
-                registry.append((Template(filename="templates/class_template.txt").render(name=usertype_name,
+                if not registries.get(filename):
+                    registries[filename] = []
+
+                registries[filename].append((Template(filename="templates/class_template.txt").render(name=usertype_name,
                                                                                           fields=usertype_fields,
                                                                                           methods=usertype_methods,
                                                                                           constructors=usertype_constructors,
-                                                                                          metas=usertype_meta_functions)))
+                                                                                          metas=usertype_meta_functions,
+                                                                                          parents=usertype_parents)))
                 global_registry.append("register_type_{}".format(i.displayname))
 
         elif i.kind == clang.cindex.CursorKind.ENUM_DECL:
@@ -99,42 +123,70 @@ def filter_node_list_by_classes(
             for child in i.get_children():
                 enum_values.append(child.displayname)
 
-            registry.append(Template(filename="templates/enum_template.txt").render(name=enum_name, values=enum_values))
+            if not registries.get(filename):
+                registries[filename] = []
+            registries[filename].append(Template(filename="templates/enum_template.txt").render(name=enum_name, values=enum_values))
             global_registry.append("register_enum_{}".format(i.displayname))
 
         elif i.kind == clang.cindex.CursorKind.FUNCTION_DECL:
+            if i.spelling.startswith("operator"):
+                continue
+            print("Speeling is {}".format(i.spelling))
             function = Method(i.spelling)
             signature = i.type.get_result().spelling + i.displayname[i.displayname.find('('):i.displayname.find(')')+1]
 
             # print("function is {}, signature {}".format(i.type.kind,signature))
             overload_check = free_functions.get(function.name)
-            if free_functions.get(function.name):
+            if overload_check:
                 overload_check.overloaded = True
                 overload_check.signatures.append(signature)
             else:
                 function.signatures.append(signature)
                 free_functions[function.name] = function
-        else:
-            if i.kind == clang.cindex.CursorKind.FUNCTION_TEMPLATE:
+
+        elif i.kind == clang.cindex.CursorKind.FUNCTION_TEMPLATE:
                 # print("III {}".format(i.spelling))
                 function = Method(i.spelling)
                 overload_check = free_functions.get(function.name)
-                if free_functions.get(function.name):
+                if overload_check:
                     overload_check.overloaded = True
                 else:
                     function.overloaded = True
                     free_functions[function.name] = function
+        else:
+            pass
+            # print("Found i.kind=".format(i.kind))
 
 
 translation_unit = index.parse(sys.argv[1], args=['-std=c++17',sys.argv[3]])
-# translation_unit = index.parse("/tmp/test.h", args=['-std=c++17',sys.argv[3]])
+# translation_unit = index.parse("/tmp/sample.h", args=['-std=c++17',sys.argv[3]])
 filter_node_list_by_classes(translation_unit.cursor.get_children())
 
-with open(sys.argv[2]+".h", 'w') as f:
-    f.write((Template(filename="templates/global_header_template.txt").render(library_name="Box2D", entries=global_registry)))
+library = "Box2D"
 
-with open(sys.argv[2]+".cpp", 'w') as f:
-    f.write("#include <"+ntpath.basename(sys.argv[2]+".h")+">\n\n")
-    f.write("".join(registry))
-    f.write((Template(filename="templates/global_cpp_template.txt").render(library_name="Box2D", entries=global_registry)))
-    f.write((Template(filename="templates/free_functions_template.txt").render(library_name="Box2D",functions=free_functions)))
+with open(sys.argv[2]+"Bind_"+library+".h", 'w') as f:
+    f.write((Template(filename="templates/global_header_template.txt").render(library_name=library, entries=global_registry)))
+
+with open(sys.argv[2]+"Bind_"+library+".cpp", 'w') as f:
+    f.write("#include <scripting/" + "Bind_" + library + ".h>\n\n")
+    f.write((Template(filename="templates/global_cpp_template.txt").render(library_name=library, entries=global_registry)))
+    f.write((Template(filename="templates/free_functions_template.txt").render(library_name=library,functions=free_functions)))
+
+for key,value in registries.items():
+    # print("Key is {}, values are {}".format(key,value))
+
+    with open(sys.argv[2] + "Bind_" + key + ".cpp", 'w') as f:
+        f.write("#include <scripting/" + "Bind_"+library+".h>\n\n")
+        f.write("".join(value))
+        # f.write((Template(filename="templates/global_cpp_template.txt").render(library_name=library,
+        #                                                                        entries=value)))
+        # f.write((Template(filename="templates/free_functions_template.txt").render(library_name=library,
+        #                                                                            functions=free_functions)))
+with open(sys.argv[2]+"bind_"+library+".cmake",'w') as f:
+    f.write("SET(BIND_"+library+"_SOURCES \n")
+    f.write("src/scripting/Bind_" + library + ".h\n")
+    f.write("src/scripting/Bind_" + library + ".cpp\n")
+    for key in registries.keys():
+        f.write("src/scripting/Bind_" + key + ".cpp\n")
+    f.write(")")
+
