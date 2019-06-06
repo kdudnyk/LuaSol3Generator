a7b5e15bcc51f1d1803b450bb6c9363adab1f773
Kostiantyn Dudnyk <ext-kostiantyn.dudnyk@here.com>
2019-06-06 10:31:59 +0300

    Implemented overloaded methods generation, temporarly disable operator bindings, split result into header and cpp

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import typing
-from pathlib import Path
 from mako.template import Template
 import sys
 import clang.cindex
+import ntpath
 
+clang.cindex.Config.set_library_file('/usr/lib/x86_64-linux-gnu/libclang-9.so.1')
 index = clang.cindex.Index.create()
-
-class Enum:
-    def __init__(self,name):
-        self.name = name;
-        self.values = []
-
-class ClassHolder:
-    def __init__(self,name):
-        self.name = name
-        self.fields = []
 
 global_registry = []
 registry = []
+
+class Method:
+    def __init__(self,name):
+        self.name = name
+        self.overloaded = False
+        self.signatures = []
+
 
 def filter_node_list_by_classes(
     nodes: typing.Iterable[clang.cindex.Cursor]
@@ -27,31 +25,57 @@ def filter_node_list_by_classes(
     for i in nodes:
         if i.kind in [clang.cindex.CursorKind.CLASS_DECL,clang.cindex.CursorKind.STRUCT_DECL]:
             if i.is_definition():
-                structHolder = ClassHolder(i.displayname)
+                usertype_name = i.displayname
+                usertype_fields = []
+                usertype_methods = {}
+
                 for child in i.get_children():
                     if child.kind == clang.cindex.CursorKind.FIELD_DECL:
-                        if child.access_specifier == clang.cindex.AccessSpecifier.PUBLIC:
-                            structHolder.fields.append(child.displayname)
+                        # we are currently interested in only public fields which are non pointers
+                        if child.access_specifier == clang.cindex.AccessSpecifier.PUBLIC and child.type.kind != clang.cindex.TypeKind.POINTER:
+                            usertype_fields.append(child.displayname)
+
                     elif child.kind == clang.cindex.CursorKind.CXX_METHOD:
                         if child.access_specifier == clang.cindex.AccessSpecifier.PUBLIC:
-                            structHolder.fields.append(child.spelling)
+                            # skip operators for now
+                            if child.spelling.startswith("operator"):
+                                continue
+                            method = Method(child.spelling)
+                            signature = child.type.get_result().spelling + child.displayname[child.displayname.find('('):child.displayname.find(')')+1]
+                            if child.is_const_method():
+                                signature += "const"
+                            print("class {} return type is {}".format(child.spelling,signature))
 
-                registry.append((Template(filename="templates/class_template.txt").render(struct=structHolder)))
+                            overload_check = usertype_methods.get(method.name)
+                            if usertype_methods.get(method.name):
+                                overload_check.overloaded = True
+                                overload_check.signatures.append(signature)
+                            else:
+                                method.signatures.append(signature)
+                                usertype_methods[method.name] = method
+
+                registry.append((Template(filename="templates/class_template.txt").render(name=usertype_name,fields=usertype_fields, methods=usertype_methods)))
                 global_registry.append("register_type_{}".format(i.displayname))
-        elif i.kind == clang.cindex.CursorKind.ENUM_DECL:
-            enum_value = Enum(i.displayname)
-            for child in i.get_children():
-                enum_value.values.append(child.displayname)
 
-            registry.append(Template(filename="templates/enum_template.txt").render(enum=enum_value))
+        elif i.kind == clang.cindex.CursorKind.ENUM_DECL:
+            enum_name = i.displayname
+            enum_values = []
+            for child in i.get_children():
+                enum_values.append(child.displayname)
+
+            registry.append(Template(filename="templates/enum_template.txt").render(name=enum_name, values=enum_values))
             global_registry.append("register_enum_{}".format(i.displayname))
 
-for filename in Path(sys.argv[1]).glob('**/*.h'):
-    translation_unit = index.parse(filename.as_posix(), args=['-std=c++17'])
-    filter_node_list_by_classes(translation_unit.cursor.get_children())
+translation_unit = index.parse(sys.argv[1], args=['-std=c++17',sys.argv[3]])
+# translation_unit = index.parse("/tmp/test.h", args=['-std=c++17',sys.argv[3]])
+filter_node_list_by_classes(translation_unit.cursor.get_children())
 
-with open(sys.argv[2], 'w') as f:
-    f.write("#include <Box2D/Box2D.h>\n")
-    f.write("#include <sol/sol.hpp>\n\n")
+with open(sys.argv[2]+".h", 'w') as f:
+
+    # f.write("".join(registry))
+    f.write((Template(filename="templates/global_header_template.txt").render(library_name="Box2D", entries=global_registry)))
+
+with open(sys.argv[2]+".cpp", 'w') as f:
+    f.write("#include <"+ntpath.basename(sys.argv[2]+".h")+">\n\n")
     f.write("".join(registry))
-    f.write((Template(filename="templates/global_register_template.txt").render(library_name="Box2D", entries=global_registry)))
+    f.write((Template(filename="templates/global_cpp_template.txt").render(library_name="Box2D", entries=global_registry)))
