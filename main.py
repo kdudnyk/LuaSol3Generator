eff26ca622d1784bd86ce2a19c9bb267536070cd
Kostiantyn Dudnyk <ext-kostiantyn.dudnyk@here.com>
2019-06-06 14:37:41 +0300

    Add some of the metafunctions and call constructors

--- main.py
+++ main.py
@@ -26,37 +26,71 @@ def filter_node_list_by_classes(
 
     for i in nodes:
         if i.kind in [clang.cindex.CursorKind.CLASS_DECL,clang.cindex.CursorKind.STRUCT_DECL]:
+            if i.is_abstract_record():
+                continue
             if i.is_definition():
                 usertype_name = i.displayname
                 usertype_fields = []
                 usertype_methods = {}
+                usertype_constructors = []
+                usertype_meta_functions = []
 
                 for child in i.get_children():
+                    if child.access_specifier != clang.cindex.AccessSpecifier.PUBLIC:
+                        continue
+
                     if child.kind == clang.cindex.CursorKind.FIELD_DECL:
                         # we are currently interested in only public fields which are non pointers
                         if child.access_specifier == clang.cindex.AccessSpecifier.PUBLIC and child.type.kind != clang.cindex.TypeKind.POINTER:
                             usertype_fields.append(child.displayname)
 
                     elif child.kind == clang.cindex.CursorKind.CXX_METHOD:
-                        if child.access_specifier == clang.cindex.AccessSpecifier.PUBLIC:
-                            # skip operators for now
-                            if child.spelling.startswith("operator"):
-                                continue
-                            method = Method(child.spelling)
-                            signature = child.type.get_result().spelling + child.displayname[child.displayname.find('('):child.displayname.find(')')+1]
-                            if child.is_const_method():
-                                signature += "const"
+                        # skip operators for now
+                        if child.spelling.startswith("operator"):
+                            arg1 = child.displayname[child.displayname.find('(')+1:child.displayname.find(')')]
+                            arg1 = arg1.replace("const",'')
+                            arg1 = arg1.replace("&",'')
+                            arg1 = arg1.replace(" ",'')
+                            arg1 += "&"
+                            arg2 = i.displayname.replace(" ",'')
+                            arg2 = arg2+"&"
 
-                            overload_check = usertype_methods.get(method.name)
-                            if usertype_methods.get(method.name):
-                                overload_check.overloaded = True
-                                overload_check.signatures.append(signature)
+                            args = []
+                            if arg1 != arg2 :
+                                args = [(arg1,arg2),(arg2,arg1)]
                             else:
-                                method.signatures.append(signature)
-                                usertype_methods[method.name] = method
+                                args = [(arg1,arg2)]
 
+                            if child.canonical.spelling == "operator*=":
+                                usertype_meta_functions.append(("*=","multiplication",args))
+                            elif child.canonical.spelling == "operator+=":
+                                usertype_meta_functions.append(("+=","addition",args))
+                            elif child.canonical.spelling == "operator-=":
+                                usertype_meta_functions.append(("-=", "subtraction",args))
+                            continue
 
-                registry.append((Template(filename="templates/class_template.txt").render(name=usertype_name,fields=usertype_fields, methods=usertype_methods)))
+                        method = Method(child.spelling)
+                        signature = child.type.get_result().spelling + child.displayname[child.displayname.find('('):child.displayname.find(')')+1]
+                        if child.is_const_method():
+                            signature += "const"
+
+                        overload_check = usertype_methods.get(method.name)
+                        if usertype_methods.get(method.name):
+                            overload_check.overloaded = True
+                            overload_check.signatures.append(signature)
+                        else:
+                            method.signatures.append(signature)
+                            usertype_methods[method.name] = method
+
+                    elif child.kind == clang.cindex.CursorKind.CONSTRUCTOR:
+                        signature = child.displayname[child.displayname.find('('):child.displayname.find(')') + 1]
+                        usertype_constructors.append(signature)
+
+                registry.append((Template(filename="templates/class_template.txt").render(name=usertype_name,
+                                                                                          fields=usertype_fields,
+                                                                                          methods=usertype_methods,
+                                                                                          constructors=usertype_constructors,
+                                                                                          metas=usertype_meta_functions)))
                 global_registry.append("register_type_{}".format(i.displayname))
 
         elif i.kind == clang.cindex.CursorKind.ENUM_DECL:
@@ -85,11 +119,7 @@ translation_unit = index.parse(sys.argv[1], args=['-std=c++17',sys.argv[3]])
 # translation_unit = index.parse("/tmp/test.h", args=['-std=c++17',sys.argv[3]])
 filter_node_list_by_classes(translation_unit.cursor.get_children())
 
-print()
-
 with open(sys.argv[2]+".h", 'w') as f:
-
-    # f.write("".join(registry))
     f.write((Template(filename="templates/global_header_template.txt").render(library_name="Box2D", entries=global_registry)))
 
 with open(sys.argv[2]+".cpp", 'w') as f:
